08f3de8be527dcde15dfb49db7db8293e6eaf973
Brayden Copley <braydenc303@gmail.com>
2018-11-11 22:04:13 -0700

    Updated the readme, and commented the javascript.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,13 +19,16 @@
 
 // Functions-----------------------------------------
 
+// When the game begins again, the player should see a new random number. 
+// Also, all the crystals will have four new hidden values. Of course, the 
+// user's score (and score counter) will reset to zero.
 function resetGame() {
     $(".crystals img:last-child").remove();
     $(".crystals img:last-child").remove();
     $(".crystals img:last-child").remove();
     $(".crystals img:last-child").remove();
     makeCrystals();
-    goal = Math.floor(((Math.random() * 100) + 1) + 19);
+    goal = Math.floor(((Math.random() * 101) + 1) + 19);
     score = 0;
     $(".goal").text(goal);
     $(".wins").text(wins);
@@ -33,12 +36,15 @@ function resetGame() {
     $(".score").text(score);
 }
 
+// There will be four crystals displayed as buttons on the page.
+// The player will be shown a random number at the start of the game.
 function makeCrystals() {
     var arr = [1,2,3,4];
     for (var i = 0; i < arr.length; i++) {
         var image = $("<img>");
         image.addClass("crystal");
-        image.attr("crystalval", Math.floor((Math.random() * 10) + 1));
+        // Each crystal should have a random hidden value between 1 - 12.
+        image.attr("crystalval", Math.floor((Math.random() * 12) + 1));
         image.attr("src", images[Math.floor(Math.random() * images.length)]);
         $(".crystals").append(image);
     }
@@ -53,19 +59,29 @@ function makeCrystals() {
 $(document).ready(function() {
     resetGame();
 
+// When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+// Your game will hide this amount until the player clicks a crystal.
+// When they do click one, update the player's score counter.
+
     $(document).on("click", ".crystal", function(){
         var val = ($(this).attr("crystalval"));
         val = parseInt(val);
         score += val;
         $(".score").text(score);
 
+        // The player wins if their total score matches the random number from the beginning of the game.
         if (score === goal) {
             wins++;
             alert("Congratulations!\nYou matched the goal of " + goal +".\nYou won that round!");
+            // The game restarts whenever the player wins or loses.
+            // The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
             resetGame();
+            // The player loses if their score goes above the random number.
         } else if (score > goal) {
             losses++;
             alert("You overshot the goal of "+ goal + ".\nYou lost that round.");
+            // The game restarts whenever the player wins or loses.
+            // The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
             resetGame();
         }
     });
